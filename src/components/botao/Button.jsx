f02f669ec0d3a57bfa0419575b534800637fb5dc
ericoborgonove <borgonove@gmail.com>
2026-03-28 15:41:42 -0400

    componente button

---
+++ src/components/botao/Button.jsx
@@ -0,0 +1,14 @@
+import styles from './Button.module.css'
+
+function Button() {
+  return (
+      <button
+        className={styles.button}
+      >
+        Clique em mim
+      </button>
+    
+  )
+}
+
+export default Button
